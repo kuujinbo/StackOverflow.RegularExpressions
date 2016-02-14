afcbcbc747fa8b7ede9bfaeaed5d218452026bb4
kuujinbo <kuujinbo@gmail.com>
2016-02-13 18:41:08 -0800

    http://stackoverflow.com/questions/35374806

---
+++ JavaScript/35374806/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('plunker', []);
+app.controller('MainCtrl', function ($scope) { });
+
+app.directive('inputRestrictor', [function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            // changed here
+            var pattern = /^(?:''||(?:[0-9]+(?:\.[0-9]{0,4})?))$/g;
+            var view_value;
+
+            function fromUser(text) {
+                var return_value;
+                if (text.match(pattern) === null) {
+                    return_value = view_value;
+                    // changed here
+                    ngModelCtrl.$setViewValue(view_value || '');
+                    ngModelCtrl.$render();
+                }
+                else {
+                    return_value = text;
+                    view_value = return_value;
+                }
+
+                return return_value;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+}]);
